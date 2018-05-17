3daad0e8a66e4c751fb0b4a8c3c7b5a5aca11120
Bogdan <treskovskiy2881@gmail.com>
2018-05-17 16:54:21 +0300

    add homework all function

--- src/measuredExecutionTime.js
+++ src/measuredExecutionTime.js
@@ -1,3 +1,10 @@
-global.measuredExecutionTime = function (fn) {
-
+global.measuredExecutionTime = function (fn, args) {
+  var result;
+  var targetFn;
+  return function () {
+    console.time(result);
+    fn = targetFn.call(args);
+    console.timeEnd(result);
+    return result;
+  };
 };
